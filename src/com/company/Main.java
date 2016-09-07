9d7e3820cf1c3edb7ee3f344eb3912b898477b81
Jacob P Gamble <jg101@pvhs-lab112-d14.pvsd.org>
2016-09-07 14:21:54 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //just talking about print
+	System.out.println("Hello world");
+        System.out.print("hey");
+        /* multi liner
+        comment
+        //single line
+        print just puts it down
+        println hits return
+         */
+        System.out.print(25);
+        System.out.println(2.0);
+    }
+}
